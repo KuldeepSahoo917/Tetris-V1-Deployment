155f239ddfecd76371dc0935d17a302609ddb772
Kuldeepsahoo917 <kuldeepsahoo917@gmail.com>
2026-06-25 05:15:35 +0000

    Deployment

---
+++ EKS_TERRAFORM/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "devsecops-tetris-kuldeep-19" # Replace with your actual S3 bucket name
+    key    = "EKS/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
